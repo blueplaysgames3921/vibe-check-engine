522d0b688d17cdf58a42c87a73ec0ebb452467ac
BLUEGAMINGGM <123081329+blueplaysgames3921@users.noreply.github.com>
2026-02-10 03:04:21 +0530

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pollinations.ai',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.pollinations.ai',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
+
